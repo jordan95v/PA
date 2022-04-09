11ece805d634919b90752006f8de70cb253c9e6d
jdr <dfrsn.jordan@icloud.com>
2022-04-09 17:35:33 +0200

    [MAIN] Added the footer. Fixed input margin when phone mode.

--- Templates/footer.php
+++ Templates/footer.php
@@ -1,3 +1,32 @@
+<footer class="bg-dark text-center text-white">
+    <div class="container p-4 pb-0">
+        <!-- Section: Social media -->
+        <section class="mb-4">
+            <!-- Facebook -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="bi bi-facebook"></i></a>
+
+            <!-- Twitter -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="bi bi-twitter"></i></a>
+
+            <!-- Google -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="bi bi-google"></i></a>
+
+            <!-- Instagram -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="bi bi-instagram"></i></a>
+
+            <!-- Github -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="bi bi-github"></i></a>
+        </section>
+    </div>
+
+    <!-- Copyright -->
+    <div class="text-center p-3" class="opacity-50">
+        © 2022 Copyright:
+        <a class=" text-white fw-bold text-decoration-none" href="#">Les Lumières</a>
+    </div>
+</footer>
+
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
--- Templates/header.php
+++ Templates/header.php
@@ -13,6 +13,11 @@ session_start()
 
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- Bootstrap icon CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="static/style.css">
 </head>
 
@@ -27,11 +32,11 @@ session_start()
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarText">
-                        <form class="d-flex ms-auto my-md-4 film-search" method="post">
+                        <form class="d-flex ms-auto  mt-4 mt-lg-2 film-search" method="post">
                             <input class="form-control search pe-5" type="search" placeholder="Chercher un film ..." aria-label="Search">
                             <button class="btn ms-5" type="submit">🔍</button>
                         </form>
-                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <ul class="navbar-nav ms-auto mt-2 mb-lg-0">
                             <li class="nav-item">
                                 <a href="#" class="nav-link">S'inscrire</a>
                             </li>
