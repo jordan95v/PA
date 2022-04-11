de74e3e4b99d4101faae0d9b1a856dd24d8c2e77
jdr <dfrsn.jordan@icloud.com>
2022-04-11 22:52:11 +0200

    [MAIN] Changed cards to link.

--- index.php
+++ index.php
@@ -16,12 +16,14 @@ include 'Templates/header.php';
     <!-- Cards for movie -->
     <div class="row py-4 row-cols-1 row-cols-lg-4 g-4 text-dark">
         <div class="col">
-            <div class="card zoom h-100 border-0">
-                <img src="Images/avengers.jpg" class="card-img-top" alt="...">
-                <div class="card-body custom-cards text-light">
-                    <h4 class="card-title text-center">Avengers</h4>
+            <a href="#" class="text-decoration-none">
+                <div class="card zoom h-100 border-0">
+                    <img src="Images/avengers.jpg" class="card-img-top" alt="...">
+                    <div class="card-body custom-cards text-light">
+                        <h4 class="card-title text-center">Avengers</h4>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
     </div>
 
