8c202cb5c3f3d3c09ed1a347dcf05cd1e7c8fd56
jordan95v <jordan.dfrsne@gmail.com>
2022-04-12 18:21:29 +0200

    Finished regsitration

--- Scripts/addUser.php
+++ Scripts/addUser.php
@@ -1,6 +1,7 @@
 <?php
 
-require 'functions.php';
+require_once 'functions.php';
+require_once 'userFunction.php';
 
 if (empty($_POST['email']) && empty($_POST['username']) &&
     empty($_POST['password']) && empty($_POST['passwordConfirm']) &&
@@ -17,15 +18,19 @@ $username = strtolower(trim($_POST['username']));
 checkUsername($username, $errors);
 checkEmail($email, $errors);
 checkPassword($_POST['password'], $_POST['passwordConfirm'], $errors);
+checkEmailExist($email, $pdo, $errors);
+checkUsernameExist($username, $pdo, $errors);
 
 if (count($errors) != 0) {
     $_SESSION['errors'] = $errors;
     header('Location: ../index.php');
 }
+else {
+    $query = $pdo->prepare("INSERT INTO petitchat_user (email, username, pwd) VALUES (:email, :username, :pwd);");
+    $pwd = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $query->execute(["email"=>$email, "username"=>$username, "pwd"=>$pwd]);
 
-$query = $pdo->prepare("INSERT INTO petitchat_user (email, username, pwd) VALUES (:email, :username, :pwd);");
-$pwd = password_hash($_POST['password'], PASSWORD_DEFAULT);
-$query->execute(["email"=>$email, "username"=>$username, "pwd"=>$pwd]);
+    $_SESSION['created'] = 1;
+    header('Location: ../index.php');
+}
 
-$_SESSION['created'] = 1;
-header('Location: ../index.php');
