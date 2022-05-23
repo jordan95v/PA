333fcb2cdb36df4bf1d48d7e1c8651fbb37da975
jordan95v <jordan.dfrsne@gmail.com>
2022-05-23 16:59:07 +0200

    Added update and removal of movies.

--- Scripts/deleteFilm.php
+++ Scripts/deleteFilm.php
@@ -4,23 +4,17 @@ require_once "functions.php";
 $pdo = connectDB();
 
 if (isAdmin($pdo)) {
-    deleteImage($pdo, $_GET["id"], $errors);
-    if (count($errors) != 0) {
-        $query = $pdo->prepare("DELETE FROM groschien_film WHERE id=:id");
-        $query->execute(["id" => $_GET["id"]]);
-        $_SESSION["deletedFilm"] = 1;
-    } else {
-        $_SESSION['errors'] = $errors;
-    }
+    deleteImage($pdo, $_GET["id"]);
+    $query = $pdo->prepare("DELETE FROM groschien_film WHERE id=:id");
+    $query->execute(["id" => $_GET["id"]]);
+    $_SESSION["deletedFilm"] = 1;
     header("Location: ../index.php");
 }
 
-function deleteImage($pdo, $id, &$errors)
+function deleteImage($pdo, $id)
 {
     $query = $pdo->prepare("SELECT image_path FROM groschien_film WHERE id=:id");
     $query->execute(["id" => $id]);
     $result = $query->fetch();
-    if (!unlink($result["image_path"])) {
-        $errors[] = "Impossible de supprimer l'image du film.";
-    }
+    unlink($result["image_path"]);
 }
