05e21a4845b24a181d3ac3c9c558418c2d22df67
jordan95v <jordan.dfrsne@gmail.com>
2022-06-21 16:20:43 +0200

    Better implementation of search bar.

--- JS/search.js
+++ JS/search.js
@@ -1,11 +1,17 @@
 function showResult() {
     let content = this.value;
     if (content.length == 0) {
+        document.querySelector('#result').remove();
         document.getElementById("livesearch").innerHTML = "";
         document.getElementById("livesearch").style.border = "0px";
         return;
     }
     var xmlhttp = new XMLHttpRequest();
+    if (!document.querySelector('#result')) {
+        let html = '<h1 id="result" class="text-light">Résultats de la recherche.</h1>';
+        document.querySelector('#livesearch').insertAdjacentHTML('beforebegin', html)
+    }
+
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("livesearch").innerHTML = this.responseText;
