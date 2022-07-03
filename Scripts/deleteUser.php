c259a41d8334ba7d630e778624a09d6a4aa7346e
jdr <dfrsn.jordan@icloud.com>
2022-07-03 14:16:26 +0200

    User can now delete their account.

--- Scripts/deleteUser.php
+++ Scripts/deleteUser.php
@@ -4,7 +4,18 @@ require "functions.php";
 
 $pdo = connectDB();
 
-if (isAdmin($pdo)) {
+if (isset($_POST["type"])) {
+    if ($_SESSION["id"] == $_POST["id"]) {
+        $query = $pdo->prepare("DELETE FROM petitchat_user WHERE id=:id;");
+        $query->execute(["id" => $_POST["id"]]);
+        updateUserLogs($pdo, $_SESSION["id"], "self deleted user id: " . $_POST["id"] . ".");
+        header("Location: ../index.php");
+    } else {
+        $_SESSION["notAdmin"] = 1;
+        header("Location: ../index.php");
+        die();
+    }
+} elseif (isAdmin($pdo)) {
     $query = $pdo->prepare("DELETE FROM petitchat_user WHERE id=:id;");
     $query->execute(["id" => $_GET["id"]]);
     updateUserLogs($pdo, $_SESSION["id"], "deleted user id: " . $_GET["id"] . ".");
